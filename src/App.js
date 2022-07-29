68c8241bfa43d2f5ee0c718bb9f26340328c8363
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-29 22:22:32 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import LoyoutComponents from "./components/LoyoutComponents";
 
 function App() {
   return (
     <div>
       <h1> React custom hooks learn with sumit</h1>
+      <LoyoutComponents/>
     </div>
   );
 }
--- src/components/LoyoutComponents.js
+++ src/components/LoyoutComponents.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const LoyoutComponents = () => {
   const [onSmallScreen, setOnSmallScreen] = useState(false);
   const checkScreenSize = () => {
-
     //window.innerWith jodi 768 er niche chole jay tiale inner with true hoye jabe
-    setOnSmallScreen(window.innerWidth< 768)
+    setOnSmallScreen(window.innerWidth < 768);
   };
+
+  useEffect(() => {
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+    return () => window.removeEventListener ('resize', checkScreenSize);
+  }, []);
+
   return (
     <div>
-      <h1>you are browsing on small device</h1>
+      <h1>you are browsing on {onSmallScreen ? 'small' : 'large'} device</h1>
     </div>
   );
 };
